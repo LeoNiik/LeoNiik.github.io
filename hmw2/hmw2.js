5d16f3f9825054b783672cf41e4de3ca1d4ed3ea
LeoNiik <leonardonicoletta32@gmail.com>
2024-10-16 08:51:24 +0200

    aggiunta frequenza relativa

--- hmw2/hmw2.js
+++ hmw2/hmw2.js
@@ -40,8 +40,9 @@ function drawLineChartArea(ctx) {
     ctx.stroke();
 }
 
-function drawPlotChartArea(x, y, width, height, ctx) {
+function drawChartArea(x, y, width, height, ctx) {
     ctx.strokeStyle = 'black';
+    ctx.lineWidth = 1;
     ctx.beginPath();
     // Disegna il rettangolo partendo dalle coordinate plotChartX, plotChartY
     ctx.rect(x, y, width, height);
@@ -53,8 +54,8 @@ function DrawAxis(ctx) {
     ctx.font = "12px Arial";  // Font per le etichette
     ctx.lineWidth = 2
 
-    drawLineChartArea(ctx);
-    drawPlotChartArea(plotChartX, plotChartY, plotWidth, plotHeight, ctx);
+    drawChartArea(lineChartX, lineChartY, lineChartWidth, lineChartHeight,ctx);
+    drawChartArea(plotChartX, plotChartY, plotWidth, plotHeight, ctx);
 
 }
 
@@ -87,10 +88,7 @@ document.addEventListener('DOMContentLoaded', async (event) => {
     ctx = ctx
     m = document.getElementById('servers').value;
     n = document.getElementById('attackers').value;
-
-    drawLineChartArea(ctx);
-
-    drawPlotChartArea(plotChartX, plotChartY, plotWidth, plotHeight, ctx);
+    DrawAxis(ctx);
 });
 
 function initializeAttackers(n,m) {
@@ -122,14 +120,20 @@ async function sim(){
     
     DrawAxis(ctx);
     let attackers = initializeAttackers(n,m);
+    let AllPaths = []
     attackersSuccesses = []
     
     for(let i = 0;i<n; i++){
-        drawTrajectory(ctx, attackers[i]);
-        plotFrequencyDistributionAtStep(attackers, Math.floor(Number(m)/2));
-        plotFrequencyDistributionAtStep(attackers, m)
-        await sleep(100);
+
+        let newPath = drawTrajectoryRelative(ctx, attackers[i]);
+        AllPaths.push(newPath);
+        ctx.stroke(newPath);
+        // reDrawTrajectories(ctx,AllPaths, attackers, i);
+        
+        // await sleep(10);
     }
+    plotFrequencyDistributionAtStep(attackers, Math.floor(Number(m)/2));
+    plotFrequencyDistributionAtStep(attackers, m)
     // for(let j=0;j<m;j++){
     //     await drawTrajectoryStep(ctx, attackers);        
     // }
@@ -142,10 +146,10 @@ async function sim(){
 
 function plotFrequencyDistributionAtStep(attackers, atStep){
     attackers.forEach(element => {
-        if(element.successesPerStep[atStep]!=0)
+        if(element.successesPerStep[atStep]!== undefined)
             attackersSuccesses.push(element.successesPerStep[atStep]);
     });
-    plotFrequencyDistribution(attackersSuccesses, false, atStep);
+    plotFrequencyDistribution(attackersSuccesses, true, atStep);
     attackersSuccesses = [];
 
 }
@@ -159,24 +163,26 @@ function plotFrequencyDistribution(arr, relative, numAttack) {
     let xPos = lineChartX+ numAttack*STEP;
     cleanPlotChartArea(xPos, plotChartY, plotWidth, plotHeight, ctx);
     
-    drawPlotChartArea(xPos, plotChartY, plotWidth, plotHeight, ctx);
+    drawChartArea(xPos, plotChartY, plotWidth, plotHeight, ctx);
 
     let frequencies = count(arr);
     
     let maxVal = arrMax(frequencies);
     let barHeightUnit = 5//plotHeight/(Number(m));  // Altezza di una unità di frequenza
-    console.log(frequencies, arr)
-    let barWidthUnit = relative ? plotWidth/m : plotWidth/maxVal;
+    // console.log(frequencies, arr)
+    let scale = relative ?  (lineChartHeight/(2*numAttack)) : STEP/4
+    let barWidthUnit = plotWidth/maxVal;
     // Disegna le barre corrispondenti alle frequenze dei valori
+    ctx.lineWidth = 7
+    ctx.strokeStyle = 'yellow';
 
     for (let i = 0; i < frequencies.length; i++) {
         let freq = frequencies[i] || 0;  // Se il valore non esiste, usa 0
         let barWidth = freq * barWidthUnit;  // Altezza della barra in base alla frequenza 
         
         let yInit = lineChartY + lineChartHeight/2
-        let yPos = yInit + i*STEP/4 - (numAttack-i)*(STEP/4) 
-        
-        ctx.strokeStyle = 'blue';
+        let yPos = yInit + i*scale - (numAttack-i)*scale
+
         drawLine(xPos, yPos,xPos + barWidth, yPos , ctx);
     }
 
@@ -220,68 +226,218 @@ function getRandomColor() {
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
+
+
+class Chart {
+
+    constructor(xPos, yPos, width, height, ctx){
+        this.x = xPos;
+        this.y = yPos;
+        this.width = width;
+        this.height = height;
+        this.ctx = ctx;
+    }
+
+    cleanChartArea(){
+        this.ctx.clearRect(this.x, this.y, this.width, this.height);
+    
+    }   
+    
+    drawPlotChartArea() {
+        this.ctx.strokeStyle = 'black';
+        this.ctx.beginPath();
+        this.ctx.rect(this.x, this.y, this.width, this.height);
+        this.ctx.stroke();
+    }
+
+    
+}
+
 class Attacker {
     constructor(id, nserv) {
         // Fields
         this.id = id;
         this.step = 0;         // Represents the current step or action
-        this.successesPerStep = Array.from({ length: nserv }, () => 0);
+        this.successesPerStep = Array.from({ length: nserv }, () => undefined);
         this.successes = 0
-        this.color = getRandomColor();
+        this.color = 'grey'//getRandomColor();
     }
 
     // Method to increase step by 1 (or a specific amount)
     incrementStep(amount = 1) {
+        this.step += amount;
         if (this.successesPerStep[this.step] === undefined) {
             this.successesPerStep[this.step] = 0;
         }
-        this.step += amount;
         this.successesPerStep[this.step] += this.successes;   
     }
 
     
-    getCoords(nAtt = this.step){
+    getCoords(nAtt = this.step, rel = false){
+
         if (this.successesPerStep[this.step] === undefined) {
             this.successesPerStep[this.step] =  0;
+        }
+        if(rel){
+            if(nAtt==0)
+                return {x : 0, y: 0}
+
+            let insuccesses = nAtt - this.successesPerStep[nAtt]
+            let relFactor = (this.successesPerStep[nAtt]/nAtt)*lineChartHeight/2 - (insuccesses/nAtt) * lineChartHeight/2;
+
+
+            return {x : nAtt * STEP, y: relFactor}
         }
         return {x : nAtt * STEP, y: this.successesPerStep[nAtt]*STEP/4 - (nAtt-this.successesPerStep[nAtt])*(STEP/4)}
     }
 
     // Method to record a success (increment successes by 1 or a specific amount)
     recordSuccess(amount = 1) {
-        if (this.successesPerStep[this.step] === undefined) {
-            this.successesPerStep[this.step] = 0;
-        }
-        this.successes+=amount
+        this.successes+=amount;
     }
 }
-function drawTrajectory(ctx, attacker){
-    let baseX = lineChartX; // Inizia dal rettangolo del line chart
-    let baseY = lineChartY+ lineChartHeight/2;
+// function drawTrajectory(ctx, attacker){
+//     let baseX = lineChartX; // Inizia dal rettangolo del line chart
+//     let baseY = lineChartY+ lineChartHeight/2;
+//     ctx.lineWidth = lineWidth;
+//     ctx.strokeStyle = attacker.color
+//     for(let i =0 ; i<m; i++){
+
+//         let coords = attacker.getCoords(i,false)
+//         // console.log(coords.x/STEP, coords.y/(STEP/4), (coords.x)/STEP + 1, coords.y, attacker.step)
+
+//         if(Math.random()>probability){       
+//             drawLine(baseX+coords.x,baseY+coords.y,baseX+coords.x+STEP,baseY+coords.y-STEP/4, ctx);
+//         }
+//         else{
+//             attacker.recordSuccess()
+//             drawLine(baseX+coords.x,baseY+coords.y,baseX+coords.x+STEP,baseY+coords.y+STEP/4, ctx);
+//         }
+//         attacker.incrementStep()
+//     }
+
+// }
+
+function drawTrajectory(ctx, attacker) {
+    let baseX = lineChartX;
+    let baseY = lineChartY + lineChartHeight / 2;
     ctx.lineWidth = lineWidth;
-    ctx.strokeStyle = attacker.color
-    for(let i =0 ; i<m; i++){
+    ctx.strokeStyle = attacker.color;
 
-        let coords = attacker.getCoords()
-        if(Math.random()>probability){       
-            drawLine(baseX+coords.x,baseY+coords.y,baseX+coords.x+STEP,baseY+coords.y-STEP/4, ctx);
+    // Creiamo un nuovo oggetto Path2D per memorizzare il percorso della traiettoria
+    let path = new Path2D();
+
+    for (let i = 0; i < m; i++) {
+        let coords = attacker.getCoords(i, false); //coordinate al tempo i
+        
+        let randomJump = (Math.random() > probability) ? -1 : 1; 
+        if(randomJump > 0) 
+            attacker.recordSuccess();
+
+        let startX = baseX + coords.x;
+        let startY = baseY + coords.y;
+        
+        let endX = startX + STEP;
+        let endY = startY + (randomJump*(STEP/4));
+        console.log((startX - baseX)/STEP, (startY - baseY)/(STEP/4), (endX - baseX)/STEP, (endY - baseY)/(STEP/4));
+        drawPathLine(startX, startY, endX, endY, path);
+        attacker.incrementStep();
+    }
+    return path
+}
+
+
+function drawTrajectoryRelative(ctx, attacker) {
+    let baseX = lineChartX; // Coordinate base del grafico
+    let baseY = lineChartY + lineChartHeight/2;
+    ctx.lineWidth = lineWidth;
+    ctx.strokeStyle = attacker.color;
+
+    // Creiamo un nuovo Path2D per memorizzare il percorso della traiettoria
+    let path = new Path2D();
+
+    for (let i = 1; i <= m; i++) {
+        // Otteniamo le coordinate relative al passo precedente (i-1) e al passo attuale (i)
+        let prevCoords = attacker.getCoords(i - 1, true); // Coordinate al tempo i-1
+
+        let randomJump = (Math.random() > probability) ? -1 : 1; 
+        if (randomJump > 0) {
+            attacker.recordSuccess(); // Registra un successo se l'attacco ha successo
         }
-        else{
-            attacker.recordSuccess()
-            drawLine(baseX+coords.x,baseY+coords.y,baseX+coords.x+STEP,baseY+coords.y+STEP/4, ctx);
-        }
-        attacker.incrementStep()
+
+        // Aggiorniamo il passo dell'attaccante
+        attacker.incrementStep();
+
+        let currCoords = attacker.getCoords(i, true);     // Coordinate al tempo i
+        // console.log(prevCoords, currCoords)
+        // Calcoliamo le coordinate assolute a partire da baseX e baseY
+        //come capire se andare verso l-alto o verso il basso
+        let startX = baseX + prevCoords.x;
+        let startY = baseY + (prevCoords.y); // Invertito per disegnare dal basso verso l'alto
+        let endX = baseX + currCoords.x;
+        let endY = baseY + (currCoords.y);   // Invertito per lo stesso motivo
+        
+        // Aggiungiamo la linea al percorso Path2D
+        drawPathLine(startX, startY, endX, endY, path);
+        // Simuliamo un attacco con probabilità di successo
     }
 
+    // Disegniamo il percorso una volta che tutte le linee sono state aggiunte
+    return path;
 }
+
+
+function createSinglePath(s) {
+    
+    currentPathNumber = s;
+    const myPath = new Path2D();
+    
+    let sumOfJumps = 0;
+    let previousY_Variate = y_Origin;
+
+    myPath.moveTo(x_Origin, y_Origin);   //visualmente facciamo partire la path dall'origine
+
+    for (let t = 1; t <= n; t++) {
+
+        sumOfJumps += myRandomJump();
+        let myProcessValue = myVariate(sumOfJumps, t);
+
+        const ascissa_t = My2dUtilities.transformX(t / n, 0, 1, rectChart.x, rectChart.width);
+
+        //const ascissa_t = My2dUtilities.transformX(t, 0, n, rectChart.x, rectChart.width);
+        const ordinata = My2dUtilities.transformY(myProcessValue, myVariate_MinView, myProcessValue_Range, rectChart.y, rectChart.height);
+
+        //scalino mantenendo quota precedente
+        myPath.lineTo(ascissa_t, previousY_Variate);
+        //salva quota per prossimo scalino
+        previousY_Variate = ordinata;
+        
+        myPath.lineTo(ascissa_t, ordinata);
+    }
+
+    return myPath;
+
+}
+
+
+function reDrawTrajectories(ctx, paths, attackers, time){
+    ctx.lineWidth = lineWidth;
+    for(let i= 0 ; i<time; i++){
+        ctx.strokeStyle = attackers[i].color;
+        ctx.stroke(paths[i]);
+    }
+}
+
+
 function drawTrajectoryStep(ctx, attackers){
     let baseX = lineChartX; // Inizia dal rettangolo del line chart
     let baseY = lineChartY+ lineChartHeight/2;
     ctx.lineWidth = lineWidth;
     attackers.forEach(element => {
         ctx.strokeStyle = element.color
-
-        let coords = element.getCoords()
+        
+        let coords = element.getCoords(true)
+        // console.log(coords)
         if(Math.random()>probability){       
             drawLine(baseX+coords.x,baseY+coords.y,baseX+coords.x+STEP,baseY+coords.y-STEP/4, ctx);
         }
@@ -306,6 +462,11 @@ function drawLine(xi, yi, xf, yf, ctx){
     // Draw the Path
     ctx.stroke();
     ctx.closePath();
+}
+
+function drawPathLine(xi, yi, xf, yf, path){
+    path.moveTo(xi,canvas.height-yi);
+    path.lineTo(xf,canvas.height-yf);
 }
 
 // dev(n) = dev(n-1) + (x(n)- x(n-1))*(x(n)-mean(n))
